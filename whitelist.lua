312d6f0c20569f1039916ca2fdc59e9f9f53d6f5
Mati278 <100154995+Mati278@users.noreply.github.com>
2022-08-23 22:40:40 -0500

    Create whitelist.lua

---
+++ whitelist.lua
@@ -0,0 +1,14 @@
+local Client = game:GetService'Players'.LocalPlayer
+local jk = Client.userId
+
+if not jk == 3664846070 then
+    Client:Kick(string.format('ur mom fat lol'))
+elseif not jk == 3634720379 then
+    Client:Kick(string.format('ur mom fat lol'))
+elseif not jk == 337089473 then
+    Client:Kick(string.format('ur mom fat lol'))
+elseif not jk == 3267163772 then
+    Client:Kick(string.format('ur mom fat lol'))
+elseif not jk == 3311810156 then
+    Client:Kick(string.format('ur mom fat lol'))
+end
